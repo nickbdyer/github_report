03aa8d38dc6f9af95b6c522842ef9482bf59d066
1278dale <dalestevens1278@gmail.com>
2014-12-02 13:15:10 +0000

    Finished

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 
+set :public_folder, Proc.new { File.join(root, "..", "public")}
+
+
 get '/' do
   response['Access-Control-Allow-Origin'] = '*'
   Time.now.to_s
+  erb :index
 end
